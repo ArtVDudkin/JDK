410619b683615d021b2e5d0d41e75b4674501770
Artem <kardan97@yandex.ru>
2023-10-31 22:44:21 +0700

    Dining philosophers simple version

--- Hometask_5/Hometask5/src/main/java/ru/geekbrains/hometask5/Program.java
+++ Hometask_5/Hometask5/src/main/java/ru/geekbrains/hometask5/Program.java
@@ -1,9 +1,20 @@
 package ru.geekbrains.hometask5;
 
-public class Program {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Program {
     public static void main(String[] args) {
 
+        int guests = 5;             // 5 философов
+        List<Philosopher> philosophers = new ArrayList<>();
+        for (int i = 1; i <= guests; i++) {
+            philosophers.add(new Philosopher("Philosopher-" + i));
+        }
+
+        Dining diningPhilosophers = new Dining(philosophers);
+
+        diningPhilosophers.setThreads();
     }
 
 }
