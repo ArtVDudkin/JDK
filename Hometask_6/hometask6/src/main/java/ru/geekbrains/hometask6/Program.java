75beb39466a0a69f4740a5629269e639b63f9608
Artem <kardan97@yandex.ru>
2023-11-21 00:42:31 +0700

    Monty Hall paradox app created

---
+++ Hometask_6/hometask6/src/main/java/ru/geekbrains/hometask6/Program.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.hometask6;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        int doors = 3;
+        int maxIteration = 1000;
+
+        MontyHall game = new MontyHall(doors, maxIteration);
+        game.startGame();
+        game.getResult();
+
+    }
+}
